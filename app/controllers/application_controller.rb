444724cd0eb9dbd99bc13c9ff9f48011565fba7f
Adam Duff <j.adam.duff@gmail.com>
2018-09-10 06:59:49 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :session_secret, "my_application_secret"
   set :views, Proc.new { File.join(root, "../views/") }
+
+  patch '/owners/:id' do
+      @owner = Owner.find(params[:id])
+      ####### bug fix
+      if !params[:owner].keys.include?("pet_ids")
+      params[:owner]["pet_ids"] = []
+      end
+      #######
+
+      @owner.update(params["owner"])
+      if !params["pet"]["name"].empty?
+        @owner.pets << Pet.create(name: params["pet"]["name"])
+      end
+      redirect "owners/#{@owner.id}"
+  end
 end
